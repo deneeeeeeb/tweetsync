73e5cea16ddd6c8e4cfddf53e3e3b8d65539fa1c
deneeeeeeb <deneeeeeeb@gmail.com>
2018-01-24 23:16:04 +0900

    refactoring

--- post_discord.py
+++ post_discord.py
@@ -1,6 +1,6 @@
 #! /home/pi/.pyenv/shims/python
 # -*- coding:utf-8 -*-
-import json, config
+import json
 import configparser
 import time
 import os
@@ -9,104 +9,106 @@ import json
 import requests
 import sys
 from pprint import pprint
-
-# import logging
-# logging.basicConfig(level=logging.DEBUG)
-
 from requests_oauthlib import OAuth1Session
 
 
-CK = config.CONSUMER_KEY
-CS = config.CONSUMER_SECRET
-AT = config.ACCESS_TOKEN
-ATS = config.ACCESS_TOKEN_SECRET
+# load config
+config   = configparser.ConfigParser()
+confpath = os.path.dirname(os.path.abspath(__file__)) + "/config.ini"
+config.read(confpath)
 
-webhook_url = config.WEBHOOK_URL
 
+# get oauth session
+CK  = config['common']['CONSUMER_KEY']
+CS  = config['common']['CONSUMER_SECRET']
+AT  = config['common']['ACCESS_TOKEN']
+ATS = config['common']['ACCESS_TOKEN_SECRET']
+twitter = OAuth1Session(CK, CS, AT, ATS)
 
 
 # debug run
 # os.system('echo "" > /tmp/splamemo.list')
 
 
+# main loop
 while True:
-    twitter = OAuth1Session(CK, CS, AT, ATS)
-    # url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
-    url = "https://api.twitter.com/1.1/lists/statuses.json"
-    # url = "https://api.twitter.com/1.1/lists/list.json"
 
-    params ={'count' : 100, 'owner_screen_name' : 'deneeeeeeb', 'slug' : 'splaplayers'}
-    # params ={'count' : 10, 'owner_screen_name' : 'deneeeeeeb', 'slug' : 'splamemo'}
+    # set twitter api
+    url    = "https://api.twitter.com/1.1/lists/statuses.json"
+    params = {'count' : 100, 'owner_screen_name' : 'deneeeeeeb', 'slug' : 'splaplayers'}
 
-    req = ""
+    # get twitter api
     req = twitter.get(url, params = params)
 
+    # get twitter api success
     if req.status_code == 200:
-        timeline = ""
-        timeline = json.loads(req.text)
 
+        # get json
+        timeline = json.loads(req.text)
        
+        # loop for each tweet
         for tweet in reversed(timeline):
 
-            cmd = 'grep ' + str(tweet['id']) + ' /tmp/splamemo.list'
-            print(cmd)
+            # check posted id
+            cmd = 'grep ' + str(tweet['id']) + ' /tmp/splamemo.list >> /dev/null'
             if os.system(cmd):
 
                 # debug
-                # print(tweet['user']['name'])
-                # print(tweet['user']['screen_name'])
-                # print(tweet['text'])
-                # print(tweet['truncated'])
                 # pprint(tweet)
-                # print("------------")
-
-                truncated = tweet['truncated']
 
                 # check truncated
-                tweet_text=""
+                truncated  = tweet['truncated']
+                tweet_text =""
+
+                # truncated
                 if truncated:
-                    pattern = r"https[^ ]*$"
-                    tweeturl = re.search(pattern , tweet['text'])
+
+                    # set text (url)
+                    pattern    = r"https[^ ]*$"
+                    tweeturl   = re.search(pattern , tweet['text'])
                     tweet_text = tweeturl.group()
+
+                # not truncated
                 else:
-                    # tweet_text = "https://twitter.com/" + tweet['user']['screen_name'] + "/status/" + str(tweet['id'])
+                    # set text
                     tweet_text = tweet['text']
 
-                # print(tweet['user']['profile_image_url_https'])
-
-                # print('----------------------------------------------------')
-
-
-
-                # post data
-                payload_dic = {
-                    # "content":str(tweeturl.group()),
-                    "content":str(tweet_text),
-                    "username":tweet['user']['name'],
-                    "avatar_url":tweet['user']['profile_image_url_https'],
+                # set post data
+                post_data = {
+                    "content"   : str(tweet_text),
+                    "username"  : tweet['user']['name'],
+                    "avatar_url": tweet['user']['profile_image_url_https'],
                 }
 
-                json_data = '{\'content\': \'test\'}'
-
                 # post
-                r = requests.post(webhook_url, data=json.dumps(payload_dic), headers={'Content-Type': 'application/json'})
-                print(r)
+                print("post : " + str(tweet['id']))
+                webhook_url = config['develop']['WEBHOOK_URL']
+                r = requests.post(webhook_url, data=json.dumps(post_data), headers={'Content-Type': 'application/json'})
+
+                # if error
                 if r.status_code != 204:
+
+                    # wait & rerun
                     time.sleep(60)
                     r = requests.post(webhook_url, data=json.dumps(payload_dic), headers={'Content-Type': 'application/json'})                    
+
+                    # error end
                     if r.status_code != 204:
+                        print("error: %d" % r.status_code)
+                        print(r.json())
                         sys.exit(10)
 
 
-
-
-                # add temp file
+                # add log
                 cmd = 'echo ' + str(tweet['id']) + ' >> /tmp/splamemo.list'
                 os.system(cmd)
 
+    # get twitter api error
     else:
-        print("ERROR: %d" % req.status_code)
+        print("error: %d" % req.status_code)
         print(req.json())
-        sys.exit(10)        
+        sys.exit(20)
 
+
+    # wait
     time.sleep(60)
